570f57f485540a9495dfbfa014bb767b6c1cc716
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-11-18 00:58:24 +0100

    WatchList --> controllers ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_103821) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_235517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,15 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_103821) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.text "comment"
     t.integer "rating"
-    t.bigint "list_id", null: false
+    t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["list_id"], name: "index_reviews_on_list_id"
+    t.string "comment"
   end
 
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
-  add_foreign_key "reviews", "lists"
 end
